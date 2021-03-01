dc294d1554a60931f1506386919e2445d37033ab
Fasterdes <keyn432des@gmail.com>
2021-03-01 10:25:47 +0700

    Initial Commit

---
+++ E-Commerce/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  E-Commerce
+//
+//  Created by Fa Ainama Caldera S on 01/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
